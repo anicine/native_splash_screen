94e0eb67529e73d40abd6c26c52fdead7ee99a13
djeddi-yacine <yacine2700dj@gmail.com>
2025-05-13 05:23:39 +0100

    add the necessary changes to run the example macos app

--- native_splash_screen/example/macos/Runner/AppDelegate.swift
+++ native_splash_screen/example/macos/Runner/AppDelegate.swift
@@ -1,13 +1,19 @@
 import Cocoa
 import FlutterMacOS
+import native_splash_screen_macos
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+    override func applicationWillFinishLaunching(_ notification: Notification) {
+        NativeSplashScreen.configurationProvider = NativeSplashScreenConfiguration()
+        NativeSplashScreen.show()
+    }
 }
