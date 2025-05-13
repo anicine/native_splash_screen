fbbe51770b802cdb5673dc393a25765d3e07d4b0
djeddi-yacine <yacine2700dj@gmail.com>
2025-05-13 03:03:02 +0100

    add generated flutter plugin to the example app

--- native_splash_screen/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ native_splash_screen/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import native_splash_screen_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  NativeSplashScreenMacosPlugin.register(with: registry.registrar(forPlugin: "NativeSplashScreenMacosPlugin"))
 }
